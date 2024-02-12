16eb60680900813b1d1f4d0c86d5ea88ed591823
killaterminal <157806879+killaterminal@users.noreply.github.com>
2024-02-12 10:28:44 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ bot.onText(/\/start/, (msg) => {
         const botName = me.first_name;
 
         const videoNoteFilePath = 'source/preview-video.mp4';
-        const videoCaption = `Hola 👋 ${userName}\n\n` +
-            `Encantado de conocerte, mi nombre es ${botName}.\n\n` +
-            'El caso es que por algo has llegado hasta aquí. Tienes unas ganas locas de ganar mucho dinero. Me encantaría ayudarte en este empeño.\n\n' +
-            'Me honra ver que mis esfuerzos marcan la diferencia en la vida de otras personas. Todos los que tenían deudas las han saldado y han empezado una nueva vida.\n\n' +
-            'Personas de mi equipo tienen ahora familias numerosas y no necesitan nada.\n\n' +
-            'Esto me hace feliz, ¡y te ayudaré a que TÚ también te hagas rico!';
+        const videoCaption = `Ciao 👋 ${userName}\n\n` +
+            `Piacere di conoscervi, il mio nome è ${botName}.\n\n` +
+            'Il fatto è che siete arrivati qui per un motivo. Avete un desiderio folle di guadagnare molto denaro. Mi piacerebbe aiutarti in questa impresa.\n\n' +
+            'Sono onorata di vedere che i miei sforzi fanno la differenza nella vita di altre persone. Tutti coloro che avevano debiti li hanno saldati e hanno iniziato una nuova vita.\n\n' +
+            'Le persone del mio team hanno famiglie numerose e non hanno bisogno di nulla.\n\n' +
+            'Questo mi rende felice, e aiuterò anche VOI ad arricchirvi!';
 
         const videoOptions = {
             caption: videoCaption,
@@ -63,8 +63,8 @@ bot.onText(/\/start/, (msg) => {
 
         const keyboard = {
             inline_keyboard: [
-                [{ text: 'Escríbeme ✍️', callback_data: 'escribeme_command', url: chatLink, }],
-                [{ text: 'Cómo funciona el programa', callback_data: 'como_funciona_el_programa' }],
+                [{ text: 'Scrivimi a ✍️', callback_data: 'escribeme_command', url: chatLink }],
+                [{ text: 'Come funziona il programma', callback_data: 'como_funciona_el_programa' }],
             ],
         };
 
@@ -78,28 +78,21 @@ bot.onText(/\/start/, (msg) => {
     });
 });
 
-
 async function comoTestimonios(chatId, callbackQuery) {
     try {
         function isPhoto(fileUrl) {
             return fileUrl.endsWith('.jpg') || fileUrl.endsWith('.jpeg') || fileUrl.endsWith('.png');
         }
-
         const reviews = await Reviews.find({});
-
         console.log('Reviews:', reviews);
-
         for (const review of reviews) {
             const fileUrl = review.file;
             const videoCaption = review.text;
-
             console.log('File URL:', fileUrl);
             console.log('Video Caption:', videoCaption);
-
             const videoOptions = {
                 caption: videoCaption,
             };
-
             if (isPhoto(fileUrl)) {
                 await bot.sendPhoto(chatId, fileUrl, videoOptions);
             } else {
@@ -109,28 +102,27 @@ async function comoTestimonios(chatId, callbackQuery) {
     } catch (error) {
         console.error('Error fetching reviews:', error);
     }
-
     bot.answerCallbackQuery(callbackQuery.id);
 }
 
 async function comoFuncionaElPrograma(chatId, callbackQuery) {
     const videoNoteFilePath = 'source/reg-video.mp4';
 
-    const videoCaption = `Es hora de cambiar tu vida ❤️🫂.\n\n` +
-        'La esencia es sencilla: la app predice desde dónde partirá el avión, y siempre lo hace con precisión. Lo que ves en la pantalla es el multiplicador por el que se multiplicará tu apuesta.\n\n' +
-        'Puedes conseguir esta app gratis durante 7 días.\n\n' +
-        'Para ello, debes aceptar nuestros acuerdos de usuario:\n\n' +
-        '1) Confirmo que no retiraré cantidades que excedan el límite permitido por el regulador de mi país.\n\n' +
-        '2) Confirmo que no tengo adicción al juego y que estoy dispuesto a hacer todo con cuidado y atención.\n\n' +
-        'Registrarse ';
+    const videoCaption = `È ora di cambiare vita ❤️🫂.\n\n` +
+        'L`essenza è semplice: l`app predice il punto di partenza dell`aereo e lo fa sempre con precisione. Quello che vedete sullo schermo è il moltiplicatore per il quale verrà moltiplicata la vostra puntata.\n\n' +
+        'È possibile ottenere questa applicazione gratuitamente per 7 giorni.\n\n' +
+        'Per farlo, è necessario accettare i nostri accordi con gli utenti:\n\n' +
+        '1) Confermo che non preleverò importi superiori al limite consentito dall`autorità di vigilanza del mio paese.\n\n' +
+        '2) Confermo di non avere dipendenza dal gioco d`azzardo e di essere disposto a fare tutto con cura e attenzione.\n\n' +
+        'Registro';
 
     const videoOptions = {
         caption: videoCaption,
         parse_mode: 'Markdown',
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Escríbeme ✍️', callback_data: 'escribeme_command', url: chatLink }],
-                [{ text: 'Testimonios', callback_data: 'testimonials' }],
+                [{ text: 'Scrivimi a ✍️', callback_data: 'escribeme_command', url: chatLink }],
+                [{ text: 'Testimonianze', callback_data: 'testimonials' }],
             ],
         },
     };
@@ -145,6 +137,8 @@ async function comoFuncionaElPrograma(chatId, callbackQuery) {
 bot.on('callback_query', (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
     const action = callbackQuery.data;
+
+    bot.sendMessage(chatId, teleg)
 
     if (action === 'como_funciona_el_programa') {
         comoFuncionaElPrograma(chatId, callbackQuery);
